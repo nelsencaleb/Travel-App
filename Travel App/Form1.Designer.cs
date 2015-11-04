83213040cb241869493db4d656855655378034db
kd5278fi <kd5278fi@go.minneapolis.edu>
2015-11-03 18:04:00 -0600

    commit

--- Travel App/Form1.Designer.cs
+++ Travel App/Form1.Designer.cs
@@ -203,6 +203,7 @@
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // dateTimePicker2
             // 
